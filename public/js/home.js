c5dd2372ae0dc6bddd19c7eee824c9d79db1e388
Anuj <anujsaheba@gmail.com>
2019-01-16 23:19:25 -0600

    added in jquery prevent default and reorganizing forms and SQL functions

---
+++ public/js/home.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     $('#author-id').submit(function(e){
+//         e.preventDefault();
+//     })
+//     $('#blog-id').submit(function(e){
+//         e.preventDefault();
+//     })
+//     $('#category-id').submit(function(e){
+//         e.preventDefault();
+//     })
+//     $('#comment-id').submit(function(e){
+//         e.preventDefault();
+//     })
+
+//     console.log('hello world');
+// })
