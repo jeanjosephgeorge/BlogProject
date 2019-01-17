2ec9b3f8fdf0e4a7993ecc7a1985425d7c49fdbe
Anuj <anujsaheba@gmail.com>
2019-01-16 21:00:24 -0600

    added author db query

--- routes/author.js
+++ routes/author.js
@@ -6,7 +6,12 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlendcoded({extended: false}));
 
 router.get('/author', (req,res)=>{
-    res.render('author')
+    db.any('SELECT * FROM author')
+    .then((data)=>{
+        res.render('author',{
+            author: data
+        })
+    });
 });
 
 router.post('/author', (req,res)=>{
@@ -14,6 +19,16 @@ router.post('/author', (req,res)=>{
     var lname = req.body.lname;
     var bio = req.body.bio;
     var imagepath = req.body.imagepath;
+
+    db.none("INSERT INTO author (fname, lname, bio, imagepath) VALUES ($1, $2, $3, $4)", fname, lname, bio, imagepath)
+    .then((data)=>{
+        res.render('author',{
+            author: data
+        })
+    });
+
+
+
 });
 
 
